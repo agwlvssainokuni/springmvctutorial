1ecfe5e1c6f4d2790fd416687707e688b721feb5
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-09-23 19:56:39 +0900

    STEP 01 ホーム画面。

---
+++ src/main/java/cherry/spring/tutorial/web/secure/home/HomeController.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.spring.tutorial.web.secure.home;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.mobile.device.site.SitePreference;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import cherry.spring.tutorial.web.PathDef;
+
+@RequestMapping(PathDef.URI_HOME)
+public interface HomeController {
+
+	@RequestMapping()
+	ModelAndView init(Authentication auth, Locale locale,
+			SitePreference sitePref, HttpServletRequest request);
+
+}
---
+++ src/main/webapp/WEB-INF/tiles/secure/home/init.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8" trimDirectiveWhitespaces="true" session="false"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="f" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="security"
+	uri="http://www.springframework.org/security/tags"%>
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
+<h2>ホーム</h2>
+<ul>
+	<li><a href="<c:url value="/secure/todo/create" />">TODO登録</a></li>
+	<li><a href="<c:url value="/secure/todo/list" />">TODO検索</a></li>
+</ul>

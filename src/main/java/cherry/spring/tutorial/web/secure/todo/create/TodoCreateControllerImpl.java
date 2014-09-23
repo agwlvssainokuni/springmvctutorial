7cb3d9ee8fa00bb337699f9661d3d55a53c00be3
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-09-23 23:26:53 +0900

    STEP 03: TODO登録画面を作成する、その1。

---
+++ src/main/java/cherry/spring/tutorial/web/secure/todo/create/TodoCreateControllerImpl.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2014 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.spring.tutorial.web.secure.todo.create;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.mobile.device.site.SitePreference;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import cherry.spring.tutorial.web.PathDef;
+
+@Controller
+public class TodoCreateControllerImpl implements TodoCreateController {
+
+	@Override
+	public TodoCreateForm getForm() {
+		TodoCreateForm form = new TodoCreateForm();
+		return form;
+	}
+
+	@Override
+	public ModelAndView init(Authentication auth, Locale locale,
+			SitePreference sitePref, HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView(PathDef.VIEW_TODO_CREATE);
+		return mav;
+	}
+
+	@Override
+	public ModelAndView confirm(TodoCreateForm form, BindingResult binding,
+			Authentication auth, Locale locale, SitePreference sitePref,
+			HttpServletRequest request) {
+
+		if (binding.hasErrors()) {
+			ModelAndView mav = new ModelAndView(PathDef.VIEW_TODO_CREATE);
+			return mav;
+		}
+
+		ModelAndView mav = new ModelAndView(PathDef.VIEW_TODO_CREATE_CONFIRM);
+		return mav;
+	}
+
+	@Override
+	public ModelAndView execute(TodoCreateForm form, BindingResult binding,
+			Authentication auth, Locale locale, SitePreference sitePref,
+			HttpServletRequest request, RedirectAttributes redirAttr) {
+
+		if (binding.hasErrors()) {
+			ModelAndView mav = new ModelAndView(PathDef.VIEW_TODO_CREATE);
+			return mav;
+		}
+
+		Integer id = 0; // 主処理をエミュレート
+
+		redirAttr.addFlashAttribute(PathDef.PATH_VAR_ID, id);
+
+		UriComponents uc = UriComponentsBuilder
+				.fromPath(PathDef.URI_TODO_CREATE)
+				.pathSegment(PathDef.SUBURI_FINISH).build();
+		ModelAndView mav = new ModelAndView();
+		mav.setView(new RedirectView(uc.toUriString(), true));
+		return mav;
+	}
+
+	@Override
+	public ModelAndView finish(Authentication auth, Locale locale,
+			SitePreference sitePref, HttpServletRequest request,
+			RedirectAttributes redirAttr) {
+		ModelAndView mav = new ModelAndView(PathDef.VIEW_TODO_CREATE_FINISH);
+		mav.addAllObjects(redirAttr.getFlashAttributes());
+		return mav;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/tiles/secure/todo/create/init.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8" trimDirectiveWhitespaces="true" session="false"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="f" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="security"
+	uri="http://www.springframework.org/security/tags"%>
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
+<h2>TODO登録</h2>
+<p>init</p>

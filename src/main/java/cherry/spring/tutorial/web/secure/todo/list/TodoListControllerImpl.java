8938ed1fce054d0ebd5dbb90068133566eddd5c2
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-10-04 11:44:15 +0900

    STEP 15

--- src/main/java/cherry/spring/tutorial/web/secure/todo/list/TodoListControllerImpl.java
+++ src/main/java/cherry/spring/tutorial/web/secure/todo/list/TodoListControllerImpl.java
@@ -16,6 +16,8 @@
 
 package cherry.spring.tutorial.web.secure.todo.list;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,9 +34,14 @@ import org.springframework.web.servlet.ModelAndView;
 import cherry.spring.common.helper.bizdate.BizdateHelper;
 import cherry.spring.common.helper.download.DownloadHelper;
 import cherry.spring.common.helper.querydsl.SQLQueryHelper;
+import cherry.spring.common.lib.util.LocalDateTimeUtil;
+import cherry.spring.common.lib.util.LocalDateUtil;
+import cherry.spring.common.type.FlagCode;
 import cherry.spring.tutorial.web.PathDef;
 import cherry.spring.tutorial.web.secure.todo.OrderBy;
 import cherry.spring.tutorial.web.secure.todo.OrderDir;
+import cherry.spring.tutorial.web.secure.todo.SearchCondition;
+import cherry.spring.tutorial.web.secure.todo.SearchResult;
 import cherry.spring.tutorial.web.secure.todo.TodoService;
 
 @Controller
@@ -93,7 +100,15 @@ public class TodoListControllerImpl implements TodoListController {
 			return mav;
 		}
 
+		SearchCondition cond = createCondition(form);
+		int pageNo = form.getPageNo();
+		int pageSz = form.getPageSz() <= 0 ? defaultPageSize : form.getPageSz();
+
+		SearchResult result = todoService.searh(auth.getName(), cond, pageNo,
+				pageSz);
+
 		ModelAndView mav = new ModelAndView(PathDef.VIEW_TODO_LIST);
+		mav.addObject(result);
 		return mav;
 	}
 
@@ -108,6 +123,29 @@ public class TodoListControllerImpl implements TodoListController {
 		}
 
 		return null;
+	}
+
+	private SearchCondition createCondition(TodoListForm form) {
+
+		SearchCondition cond = new SearchCondition();
+		cond.setPostedFrom(LocalDateTimeUtil.rangeFrom(form.getPostedFrom()));
+		cond.setPostedTo(LocalDateTimeUtil.rangeTo(form.getPostedTo()));
+		cond.setDueDateFrom(LocalDateUtil.rangeFrom(form.getDueDateFrom()));
+		cond.setDueDateTo(LocalDateUtil.rangeTo(form.getDueDateTo()));
+
+		List<FlagCode> doneFlg = new ArrayList<>();
+		if (form.isDone()) {
+			doneFlg.add(FlagCode.TRUE);
+		}
+		if (form.isNotDone()) {
+			doneFlg.add(FlagCode.FALSE);
+		}
+		cond.setDoneFlg(doneFlg);
+
+		cond.setOrderBy(form.getOrderBy());
+		cond.setOrderDir(form.getOrderDir());
+
+		return cond;
 	}
 
 }

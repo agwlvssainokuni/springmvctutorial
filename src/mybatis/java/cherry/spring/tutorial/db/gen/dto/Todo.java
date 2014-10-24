96176cb1e8b4fa597b36f86a366d0b1c60e5491a
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-10-25 06:47:27 +0900

    step-00 プロジェクト構成見直し。

--- src/mybatis/java/cherry/spring/tutorial/db/gen/dto/Todo.java
+++ src/mybatis/java/cherry/spring/tutorial/db/gen/dto/Todo.java
@@ -1,7 +1,7 @@
 package cherry.spring.tutorial.db.gen.dto;
 
-import cherry.spring.common.type.DeletedFlag;
-import cherry.spring.common.type.FlagCode;
+import cherry.spring.fwcore.type.DeletedFlag;
+import cherry.spring.fwcore.type.FlagCode;
 import java.io.Serializable;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
